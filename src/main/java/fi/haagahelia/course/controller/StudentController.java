60958e5dd6774f9825d3477a43120a4a1fa9ed79
Juha Hinkula <hinkula@gmail.com>
2016-06-19 14:21:19 +0300

    first commit

---
+++ src/main/java/fi/haagahelia/course/controller/StudentController.java
@@ -0,0 +1,47 @@
+package fi.haagahelia.course.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fi.haagahelia.course.model.Student;
+import fi.haagahelia.course.repository.StudentRepository;
+
+@Controller
+public class StudentController {
+	@Autowired
+    private StudentRepository repository; 
+	
+	@RequestMapping("/")
+	public String index(Model model) {
+		 List<Student> students = (List<Student>) repository.findAll();
+		
+		model.addAttribute("students", students);
+		
+    	return "index";
+    }
+
+    @RequestMapping(value = "add")
+    public String addStudent(Model model){
+    	model.addAttribute("student", new Student());
+        return "addStudent";
+    }	
+	
+    @RequestMapping(value = "save", method = RequestMethod.POST)
+    public String save(Student student){
+        repository.save(student);
+    	return "redirect:/";
+    }
+    
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String editRemoveEmployee(@PathVariable("id") Long studentId, Model model) {
+    	repository.delete(studentId);
+        return "redirect:/";
+    }    
+}
